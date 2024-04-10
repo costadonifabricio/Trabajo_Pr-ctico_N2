f7402a8ddc626f0252f06ad8e0c4d7d3064377b5
Fabri Cost <you@example.com>
2024-04-10 15:32:47 -0300

    Se soluciono los datos faltantes

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ except MySQLdb.Error as e:
 # Definir la sentencia SQL para crear la tabla
 sql = """
 CREATE TABLE provincias (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     provincia VARCHAR(255),
+    id VARCHAR(255),
     localidad VARCHAR(255),
-    cp INT,
-    id_prov_mstr INT
+    cp VARCHAR(255),
+    id_prov_mstr VARCHAR(255)
 )
 """
 
@@ -37,8 +37,6 @@ sql_import = f"""
 LOAD DATA INFILE '{csv_file}'
 INTO TABLE provincias
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\\n'
-IGNORE 1 ROWS
 (provincia, id, localidad, cp, id_prov_mstr)
 """
 
